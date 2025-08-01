85cd04628e833d4ed10c005f8999654ee6b478fa
Raniamea <raniamea@gmail.com>
2025-08-01 10:52:28 +0000

    Coommit Changes to GIT

--- django_app/VideoWebsite/settings.py
+++ django_app/VideoWebsite/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media file (uploads) handling
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/content/drive/MyDrive/arabic-video-summarisation/videos'
