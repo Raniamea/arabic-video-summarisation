d8c0f8e907c31ba25660fc69dd074c2003a6b2ef
Raniamea <raniamea@gmail.com>
2025-08-01 11:00:04 +0000

    Commit Changes to GIT

--- django_app/uploader/urls.py
+++ django_app/uploader/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_video, name='upload_video'),
+    path('', views.home, name='home'),  # This handles the root "/"
+    path('upload/', views.upload_video, name='upload_video'),  # You can keep this if you already have it
 ]
