c1cb8c0dff8bc8ed3eebba21a825b4ed87454bb6
Raniamea <raniamea@gmail.com>
2025-08-01 10:38:26 +0000

    Explain what you changed

--- django_app/VideoWebsite/settings.py
+++ django_app/VideoWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9g3x2^-%c&bs5o#cv*wo8a8r$vlt_qrzir3c$a1&vct5e$d%qr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
